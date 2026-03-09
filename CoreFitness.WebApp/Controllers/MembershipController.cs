c0cd7e0647c7469f09304be83fdb41b319011fbe
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-03-09 13:18:25 +0100

    add hero

---
+++ CoreFitness.WebApp/Controllers/MembershipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.WebApp.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

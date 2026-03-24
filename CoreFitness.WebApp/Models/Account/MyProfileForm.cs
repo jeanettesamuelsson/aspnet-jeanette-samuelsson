7638af21b1f5adf753b74697b875b07fb67a7370
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-03-24 14:51:52 +0100

    feat: Add account/about form model and view model

---
+++ CoreFitness.WebApp/Models/Account/MyProfileForm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitness.WebApp.Models.Account;
+
+public class MyProfileForm
+{
+    [Required(ErrorMessage ="First name is required")]
+    [Display(Name ="First Name", Prompt ="Please Enter Your First Name")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is required")]
+    [Display(Name = "Last Name", Prompt = "Please Enter Your Last Name")]
+    public string LastName { get; set; } = null!;
+
+    [Phone]
+    [Display(Name = "Phone Number", Prompt = "(optional) Please Enter Your Phone Number")]
+    public string? PhoneNumber { get; set; } = null!;
+
+    [Url]
+    [Display(Name = "Profile Image", Prompt = "(optional) Upload Profile Image")]
+    public string? ProfielImageUri { get; set; }
+
+
+
+}

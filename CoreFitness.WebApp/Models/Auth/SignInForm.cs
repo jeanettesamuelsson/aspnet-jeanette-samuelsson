65eb72d9096947e41cdcad589b40f9fa8714bf36
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-03-24 14:39:03 +0100

    feat: Add authentication form models

---
+++ CoreFitness.WebApp/Models/Auth/SignInForm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitness.WebApp.Models.Auth;
+
+public class SignInForm
+{
+    [Required(ErrorMessage = "Email address is required")]
+    [Display(Name = "Email Address", Prompt = "Please Enter Email Address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Please Enter Password")]
+    public string Password { get; set; } = null!;
+    public bool RemeberMe { get; set; }
+
+
+
+
+}

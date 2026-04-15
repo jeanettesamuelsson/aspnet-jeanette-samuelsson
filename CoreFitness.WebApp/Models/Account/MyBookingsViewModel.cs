18b061e6bcb35678adb02803a8c5da77a83fa7f1
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-04-15 14:26:26 +0200

    Add MyBookingsView with basic styling

---
+++ CoreFitness.WebApp/Models/Account/MyBookingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreFitness.WebApp.Models.Account;
+
+public class MyBookingsViewModel
+{
+    public List<BookedClassViewModel> BookedClasses { get; set; } = [];
+}
+
+public class BookedClassViewModel
+{
+    public string BookingId { get; set; } = null!;
+    public string ClassName { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public string Instructor { get; set; } = null!;
+}

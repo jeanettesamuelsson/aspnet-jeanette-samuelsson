a94a525c9547b04862db1c960ad097980a355101
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-04-28 10:32:04 +0200

    feat: add contactform controller and viewmodel

--- CoreFitness.WebApp/Controllers/CustomerServiceController.cs
+++ CoreFitness.WebApp/Controllers/CustomerServiceController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreFitness.WebApp.Models.CustomerService;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CoreFitness.WebApp.Controllers
+
+
+namespace CoreFitness.WebApp.Controllers;
+
+public class CustomerServiceController : Controller
 {
-    public class CustomerServiceController : Controller
+
+    //send in empty model 
+    [HttpGet]
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
+        
+        return View(new ContactFormViewModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken] 
+    public IActionResult Index(ContactFormViewModel form)
+    {
+        if (!ModelState.IsValid)
+        { //reurn form view with error message if modelstate is not valid
+            return View(form);
         }
+
+        TempData["SuccessMessage"] = "Thank you for contacting us! We will get back to you soon.";
+
+        return RedirectToAction("Index");
     }
 }
---
+++ CoreFitness.WebApp/Models/CustomerService/ContactFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreFitness.WebApp.Models.CustomerService;
+
+public class ContactFormViewModel
+{
+    [Display(Name = "Firstname", Prompt = "Plase enter first name")]
+    [Required(ErrorMessage = "First name is required")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Lastname", Prompt = "Plase enter last name")]
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Email", Prompt = "Plase enter email")]
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Please write a message")]
+    [StringLength(500, ErrorMessage = "Message is too long")]
+    public string Message { get; set; } = null!;
+}

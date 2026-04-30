a2905726196f92f5e49f8104ba826cb94d6ee65b
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2026-04-30 10:44:20 +0200

    fix admin controller and add a view for admin page

---
+++ CoreFitness.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFitness.WebApp.Controllers;
+
+public class AdminController : Controller
+{
+
+    //test for admin 
+    [Authorize(Roles = "Admin")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //// show all bookings
+    //public async Task<IActionResult> Bookings()
+    //{
+    //    return View();
+    //}
+
+    //// create a form to add new gym classes
+    //public IActionResult CreateSession()
+    //{
+    //    return View();
+    //}
+
+}
